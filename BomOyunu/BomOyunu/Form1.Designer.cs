9aeee143d37fe182fadb2ecd8db24dd43311f880
enesfurkancali <67002809+enesfurkancali@users.noreply.github.com>
2021-06-26 14:47:42 +0300

    .

---
+++ BomOyunu/BomOyunu/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace BomOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.BackColor = System.Drawing.Color.Green;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(91, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 173);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BOM";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rightButton.Location = new System.Drawing.Point(214, 356);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(75, 43);
+            this.rightButton.TabIndex = 1;
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // leftButton
+            // 
+            this.leftButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.leftButton.Location = new System.Drawing.Point(95, 356);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(75, 43);
+            this.leftButton.TabIndex = 2;
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(375, 35);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // start
+            // 
+            this.start.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.start.Location = new System.Drawing.Point(95, 485);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(194, 39);
+            this.start.TabIndex = 4;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(375, 566);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BOM";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BomOyunu/BomOyunu/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BomOyunu
+{   
+    public partial class Form1 : Form
+    {
+        public int number { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            var path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddEllipse(0, 0, label1.Width, label1.Height);
+            this.label1.Region = new Region(path);
+            leftButton.Enabled = false;
+            rightButton.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        int time = 0;
+        private void start_Click(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Green;
+            number = 1;
+            leftButton.Text =""+ (number + 1);
+            rightButton.Text = "BOM";
+            label1.Text = ""+number;
+            start.Enabled = false;
+            leftButton.Enabled = true;
+            rightButton.Enabled = true;
+            time = 5;
+            progressBar1.Maximum = 5001;
+            progressBar1.Value = time*1000;
+            timer1.Start();
+        }
+
+        Random random = new Random();
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            if (bomControl(Convert.ToInt32(leftButton.Text)) == 1)
+                gameOver();
+            else
+            {
+                nextStep();
+                int r = random.Next(10);
+                if (r % 2 == 0)
+                    changeButtonsRandom();
+            }
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            if (bomControl(Convert.ToInt32(leftButton.Text)) == 0)
+                gameOver();
+            else
+            {
+                nextStep();
+                int r = random.Next(10);
+                if (r % 2 == 0)
+                    changeButtonsRandom();
+            }
+        }       
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (time > 0)
+            { 
+                time--;
+                progressBar1.Value = time * 1000;
+            }
+            if (time == 0)
+                gameOver();           
+        }
+
+        public int bomControl(int x)
+        {
+            if (x % 5 == 0)
+                return 1;
+            else
+                return 0;
+        }
+
+        public void gameOver()
+        {
+            label1.BackColor = Color.Red;
+            timer1.Stop();
+            progressBar1.Value = 0;
+            MessageBox.Show("GAME OVER!");
+            leftButton.Enabled = false;
+            rightButton.Enabled = false;
+            start.Enabled = true;
+            label1.Text = "Play Again";
+        }
+
+        public void changeButtonsRandom()
+        {
+            Point t = rightButton.Location;
+            rightButton.Location = leftButton.Location;
+            leftButton.Location = t;
+        }
+
+        public void nextStep()
+        {
+            time = 5;
+            progressBar1.Value = time * 1000;
+            number += 2;
+            if (bomControl(number) == 1)
+            {
+                label1.Text = "BOM";
+                leftButton.Text = "" + (number + 1);
+            }
+            else
+            {
+                label1.Text = "" + (number);
+                leftButton.Text = "" + (number + 1);
+            }
+        }
+
+    }
+}
